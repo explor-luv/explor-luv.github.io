4100bb2a45c3ae5f3716f50e94c2236e294d5029
explor-luv <explor.luv@gmail.com>
2016-08-10 21:19:10 -0400

    Add files via upload

---
+++ AddCity.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Add A City </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel= "stylesheet" href="ExploR.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css">
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+
+    .jumbotron {
+      height: 100%;
+      background-attachment: fixed;
+      background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+
+
+
+  </style>
+</head>
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html">ExploR</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li><a href="index.html">Cities</a></li>
+          <li><a href="About.html">About</a></li>
+          <li class = "active"><a href="http://localhost:8888/AddCity.php">Add A City</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <section class="jumbotron">
+    <div class="container text-center">
+        <h1><img src = "logo_website.png" height = "250" width = "250"></h1>
+        <p><font color = "black">Find the World. Discover the World. Enjoy the World.</p>
+          <div class = "search-input-holder">
+  </section>
+
+
+
+
+<div id="feedback">
+<!-- Heading Of The Form -->
+<div class="head">
+<h3 style = "text-align: center;">Add A City or Add A Location</h3>
+<p style="text-align: center;">Send us the name of your city and we will work on adding it! Or you could send us locations in your city!</p>
+</div>
+<!-- Feedback Form -->
+<form method="post" action="#" id="form" name="form">
+      <input class="w3-input w3-border w3-hover-light-grey" name= "vname" type="text" placeholder="Name">
+
+      <input class="w3-input w3-border w3-hover-light-grey" name="vemail" type="text" placeholder="Email">
+
+  <textarea <input class="w3-input w3-border w3-hover-light-grey" name="msg" type="text" rows="5" cols="25" placeholder="Comment"></textarea>
+<input id="send" name= "submit" type="submit" value="Submit" class="w3-btn w3-section w3-right" style="float:left!important">
+</div>
+</div>
+</div>
+
+</form>
+
+
+<h3><?php
+if(isset($_POST["submit"])){
+// Checking For Blank Fields..
+if($_POST["vname"]==""||$_POST["vemail"]==""||$_POST["msg"]==""){
+echo "Fill All Fields..";
+}else{
+// Check if the "Sender's Email" input field is filled out
+$email=$_POST['vemail'];
+// Sanitize E-mail Address
+$email =filter_var($email, FILTER_SANITIZE_EMAIL);
+// Validate E-mail Address
+$email= filter_var($email, FILTER_VALIDATE_EMAIL);
+if (!$email){
+echo "Invalid Sender's Email";
+}
+else{
+$message = $_POST['msg'];
+$headers = 'From:'. $email2 . "\r\n"; // Sender's Email
+$headers .= 'Cc:'. $email2 . "\r\n"; // Carbon copy to Sender
+// Message lines should not exceed 70 characters (PHP rule), so wrap it
+$message = wordwrap($message, 70);
+// Send Mail By PHP Mail Function
+mail("explor.luv@gmail.com", $message, $headers);
+echo "Your mail has been sent successfuly ! Thank you for your input";
+}
+}
+}
+?></h3>
+</div>
+
+<div class="well" style="backgroud-color: #ffff99;">
+ <p style ="color:red;"> **</p><p style="color:blue;"> Note: There may be locations in your city filed under other cites that are in close vicinity. If your city is missing, check cities that are close by and see if there are any locations in your area. If you still cannot find anything, contact us and add your city! </p>
+   <br>
+<p style ="color:red;">***</p><p style="color:blue;"> Also Note: It may take us a little while to add a new city or a new location. Please be patient :-)</p>
+
+</div>
+
+
+<footer class="container-fluid text-center">
+<p> @ 2016 ExploR, Luv Inc.</p>
+</footer>
+</body>
+</html>
